53e85110db302820486f7a49881913a60e994323
Cedric Jezequel <cedric.jezequel@insa-rennes.fr>
2015-10-22 11:14:24 +0200

    initial commit

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,12 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
